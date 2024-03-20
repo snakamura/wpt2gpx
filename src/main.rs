733392b06f950a7b5ecf47e2a9e251bfb3faab2a
Satoshi Nakamura <snak@snak.org>
2024-03-20 21:22:59 +0900

    Add wpt parser

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
 use std::io;
-use std::io::Read;
+use std::io::{Read, Write};
 
 mod gpx;
 mod waypoint;
 mod wpt;
 
 fn main() -> io::Result<()> {
-    let waypoints2 = vec![
-        waypoint::Waypoint {
-            name: "AAT054".to_string(),
-            latitude: 36.276783,
-            longitude: 140.145417,
-            altitude: 540,
-            description: "ASIO HG TO".to_string(),
-        },
-        waypoint::Waypoint {
-            name: "ACT052".to_string(),
-            latitude: 36.269475,
-            longitude: 140.141306,
-            altitude: 524,
-            description: "COO TO".to_string(),
-        },
-    ];
+    let mut wpt: String = String::new();
+    io::stdin().read_to_string(&mut wpt)?;
 
-    let gpx = gpx::formatter::format(&waypoints2).unwrap();
-    let x = String::from_utf8(gpx).unwrap();
-    println!("{}", x);
+    let waypoints = wpt::parser::parse(wpt.as_str()).unwrap();
+
+    let gpx = gpx::formatter::format(&waypoints).unwrap();
+    io::stdout().write(&gpx)?;
 
     Ok(())
 }
---
+++ src/wpt/parser.rs
@@ -0,0 +1,85 @@
+use crate::waypoint::Waypoint;
+use nom::bytes::complete::tag;
+use nom::character::complete::{
+    alphanumeric1, char, line_ending, not_line_ending, one_of, space1, u16,
+};
+use nom::combinator::{all_consuming, map};
+use nom::multi::many0;
+use nom::sequence::{preceded, tuple};
+use nom::IResult;
+
+pub fn parse(input: &str) -> Result<Vec<Waypoint>, String> {
+    match all_consuming(waypoints)(input) {
+        Ok((_, waypoints)) => Ok(waypoints),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
+fn waypoints(input: &str) -> IResult<&str, Vec<Waypoint>> {
+    preceded(header, many0(waypoint))(input)
+}
+
+fn header(input: &str) -> IResult<&str, ()> {
+    map(tuple((tag("$FormatGEO"), line_ending)), |_| ())(input)
+}
+
+fn waypoint(input: &str) -> IResult<&str, Waypoint> {
+    map(
+        tuple((
+            name,
+            space1,
+            latitude,
+            space1,
+            longitude,
+            space1,
+            altitude,
+            space1,
+            description,
+            line_ending,
+        )),
+        |(name, _, latitude, _, longitude, _, altitude, _, description, _)| Waypoint {
+            name: name.to_string(),
+            latitude,
+            longitude,
+            altitude,
+            description: description.to_string(),
+        },
+    )(input)
+}
+
+fn name(input: &str) -> IResult<&str, &str> {
+    alphanumeric1(input)
+}
+
+fn latitude(input: &str) -> IResult<&str, f64> {
+    map(tuple((one_of("NS"), space1, degree)), |(ns, _, degree)| {
+        let b = if ns == 'N' { 1f64 } else { -1f64 };
+        b * degree
+    })(input)
+}
+
+fn longitude(input: &str) -> IResult<&str, f64> {
+    map(tuple((one_of("EW"), space1, degree)), |(ns, _, degree)| {
+        let b = if ns == 'E' { 1f64 } else { -1f64 };
+        b * degree
+    })(input)
+}
+
+fn degree(input: &str) -> IResult<&str, f64> {
+    map(
+        tuple((u16, space1, u16, space1, u16, char('.'), u16)),
+        |(degrees, _, minutes, _, seconds, _, milliseconds)| {
+            f64::from(degrees)
+                + f64::from(minutes) / 60f64
+                + (f64::from(seconds) + f64::from(milliseconds) / 100f64) / 60f64 / 60f64
+        },
+    )(input)
+}
+
+fn altitude(input: &str) -> IResult<&str, u16> {
+    u16(input)
+}
+
+fn description(input: &str) -> IResult<&str, &str> {
+    not_line_ending(input)
+}
